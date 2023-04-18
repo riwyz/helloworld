48e813091ed04dbffbff815c98dec1372b4a88d5
Your Name <you@example.com>
2023-04-18 15:08:15 +0700

    +1 console.writline

---
+++ hello/hello/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oh, here we go again");
+            Console.WriteLine("Oh, here we go again too");
+            Console.ReadKey();
+        }
+    }
+}
